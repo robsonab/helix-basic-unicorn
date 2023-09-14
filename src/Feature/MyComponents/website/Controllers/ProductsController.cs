b4f8c626ff0c3942decdf262796135bdd7be01fe
Robson Barbosa <116354613+robson-barbosa@users.noreply.github.com>
2023-09-14 18:32:47 -0300

    Initial setup / Add new project MyComponents

---
+++ src/Feature/MyComponents/website/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Web.Mvc;
+using BasicCompany.Feature.MyComponents.Services;
+using Sitecore.Mvc.Presentation;
+
+namespace BasicCompany.Feature.MyComponents.Controllers
+{
+    public class ProductsController : Controller
+    {
+        
+        public ProductsController()
+        {            
+        
+        }
+
+        public ActionResult HeroBanner()
+        {
+            //tESTE
+            return View();
+        }
+    }
+}
---
+++ src/Feature/MyComponents/website/Services/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+
+namespace BasicCompany.Feature.MyComponents.Services
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Item> GetProducts(Item parent);
+    }
+}
