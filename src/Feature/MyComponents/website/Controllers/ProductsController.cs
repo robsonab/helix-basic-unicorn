17ae0a2e97d5fed3c5d9718377e6fb1910e4a864
Robson Barbosa <116354613+robson-barbosa@users.noreply.github.com>
2023-09-18 09:38:21 -0300

    Hero Banner

--- src/Feature/MyComponents/website/Controllers/ProductsController.cs
+++ src/Feature/MyComponents/website/Controllers/ProductsController.cs
@@ -2,6 +2,9 @@
 using System.Web.Mvc;
 using BasicCompany.Feature.MyComponents.Services;
 using Sitecore.Mvc.Presentation;
+using BasicCompany.Feature.MyComponents.Model;
+using Sitecore.Data.Fields;
+using Sitecore.Resources.Media;
 
 namespace BasicCompany.Feature.MyComponents.Controllers
 {
@@ -15,8 +18,19 @@ namespace BasicCompany.Feature.MyComponents.Controllers
 
         public ActionResult HeroBanner()
         {
-            //tESTE
-            return View();
+            var heroBannerItem = RenderingContext.CurrentOrNull.Rendering.Item;
+            var result = new HeroBannerModel();
+
+            ImageField imageField = heroBannerItem.Fields["Body Image"];
+            if (imageField == null || imageField.MediaItem == null)
+            {
+                result.ImageBackground = "";
+            }
+            else
+            {
+                result.ImageBackground = MediaManager.GetMediaUrl(imageField.MediaItem);
+            }
+            return View(result);
         }
     }
 }
---
+++ src/Feature/MyComponents/website/Model/HeroBannerModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BasicCompany.Feature.MyComponents.Model
+{
+    public class HeroBannerModel
+    {
+        public string ImageBackground { get; set; }
+    }
+}
