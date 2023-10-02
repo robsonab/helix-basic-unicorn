095f0323edeb6043ffda1b2be767847a9e8e6b69
Robson Barbosa <116354613+robson-barbosa@users.noreply.github.com>
2023-10-02 16:06:36 -0300

    Carousel V.2

--- src/Feature/MyComponents/website/Controllers/ProductsController.cs
+++ src/Feature/MyComponents/website/Controllers/ProductsController.cs
@@ -5,15 +5,22 @@ using Sitecore.Mvc.Presentation;
 using BasicCompany.Feature.MyComponents.Model;
 using Sitecore.Data.Fields;
 using Sitecore.Resources.Media;
+using Sitecore.Web.UI.WebControls;
+using System.Collections.Generic;
+using System.Linq;
+using Sitecore.Mvc.Extensions;
+using System;
+using Sitecore.Globalization;
+using System.Web.UI.WebControls;
 
 namespace BasicCompany.Feature.MyComponents.Controllers
 {
     public class ProductsController : Controller
     {
-        
+
         public ProductsController()
-        {            
-        
+        {
+
         }
 
         public ActionResult HeroBanner()
@@ -21,7 +28,7 @@ namespace BasicCompany.Feature.MyComponents.Controllers
             var heroBannerItem = RenderingContext.CurrentOrNull.Rendering.Item;
             var result = new HeroBannerModel();
 
-            ImageField imageField = heroBannerItem.Fields["Body Image"];
+            Sitecore.Data.Fields.ImageField imageField = heroBannerItem.Fields["Body Image"];
             if (imageField == null || imageField.MediaItem == null)
             {
                 result.ImageBackground = "";
@@ -32,9 +39,52 @@ namespace BasicCompany.Feature.MyComponents.Controllers
             }
             return View(result);
         }
+
+
         public ActionResult Carousel()
         {
-            return View(RenderingContext.CurrentOrNull.Rendering.Item);
+            var model = new CarouselModel();
+            List<Slide> slides = new List<Slide>();
+            var dataSource = RenderingContext.Current?.Rendering.Item;
+            if (dataSource == null) { return View(); }
+            CheckboxField showLabelsField = dataSource.Fields["Show Labels"];
+
+            model.Previous = Translate.TextByDomain("Company Dictionary", "Previous");
+            model.Next = Translate.TextByDomain("Company Dictionary", "Next");
+            model.ShowLabels = showLabelsField != null && showLabelsField.Checked;
+
+            MultilistField slidesField = dataSource.Fields["Slides"];
+
+            if (slidesField?.Count > 0)
+            {
+                var slideItems = slidesField.GetItems();
+                foreach (var slideItem in slideItems)
+                {
+                    var title = new MvcHtmlString(FieldRenderer.Render(
+                            slideItem, "Slide Title"));
+
+                    var subTitle = new MvcHtmlString(FieldRenderer.Render(
+                        slideItem, "Slide Body"));
+
+                    var image = new MvcHtmlString(FieldRenderer.Render(
+                            slideItem, "Slide Image", "class=img-fluid"));
+
+                    var callToAction = new MvcHtmlString(FieldRenderer.Render(
+                        slideItem, "Slide Link",
+                        "class=btn animated fadeInUp"));
+
+                    slides.Add(new Slide
+                    {
+                        Title = title,
+                        SubTitle = subTitle,
+                        Image = image,
+                        CallToAction = callToAction
+                    });
+                }
+                model.Slides = slides;
+            }
+
+            return View(model);
         }
     }
 }
---
+++ src/Feature/MyComponents/website/Model/SlideViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BasicCompany.Feature.MyComponents.Model
+{
+    public class CarouselModel
+    {
+        public List<Slide> Slides { get; set; }
+        public string Previous { get; set; }
+        public string Next { get; set; }
+        public bool ShowLabels { get; set; }
+    }
+
+    public class Slide
+    {
+        public MvcHtmlString Title { get; set; }
+        public MvcHtmlString SubTitle { get; set; }
+        public MvcHtmlString Image { get; set; }
+        public MvcHtmlString CallToAction { get; set; }
+
+    }
+}
